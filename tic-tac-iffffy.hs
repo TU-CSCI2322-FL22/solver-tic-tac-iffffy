06d88610a8c605cb3244a6ab0ac9015c5d0e0895
Matvei Popov <mpopov@trinity.edu>
2022-11-02 10:05:00 -0700

    Types

---
+++ tic-tac-iffffy.hs
@@ -0,0 +1,22 @@
+type Board = [[ Cell ]]
+type MiniBoard = [ Maybe Player ]
+type BigBoard = [MiniBoard] 
+Cell = Maybe Player
+data Player = Cross | Circle deriving (Show, Eq)
+
+type MBWinner = (Maybe Player, MiniBoard)
+type BBWinner =  (Maybe Player, [MBWinner]) -– (Maybe Player, BigBoard)
+
+Nothing -> let newBoards =  updateWinners x board
+                                          in if thereIsWinner then celebrate
+                                              else waitForNewMove
+                        Just x     -> error “Overwriting move”
+
+possibleWins = [[0,1,2],[3,4,5],[6,7,8],
+                           [0,3,6],[1,4,7],[2,5,8],
+                           [0,4,8],[2,4,6]]
+
+type Location = (BigBoardIndex :: Int, MiniBoardIndex :: Int)
+
+updateBoardWinners (bigLoc, smallLoc) board =
+  let miniboard = board !! bigLoc
