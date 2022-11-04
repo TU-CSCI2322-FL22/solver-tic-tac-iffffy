e184515045f4c0c3cc3808d554de8aa0b2c642e6
Matvei Popov <mpopov@trinity.edu>
2022-11-04 09:53:01 -0700

    Update tic-tac-iffffy.hs

--- tic-tac-iffffy.hs
+++ tic-tac-iffffy.hs
@@ -15,7 +15,8 @@ type Turn = Player
 type BigBoardIndex = Int
 type MiniBoardIndex = Int
 type GameState = (Turn, BigBoard)
-type Outcome = (Player, MiniBoard)
+--type Outcome = (Player, MiniBoard)
+type Outcome = Cross | Circle | Ongoing
 type Location = (BigBoardIndex, MiniBoardIndex)
 
 possibleWins = [[0,1,2],[3,4,5],[6,7,8],
