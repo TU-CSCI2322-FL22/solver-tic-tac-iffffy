0d7a7eb348e47e37ca5893f2ef3ef71cf4adb0ff
Kenneth Cadungog <kcadungo@trinity.edu>
2022-11-04 12:10:35 -0500

    Co-authored-by: Jose Angel Ayala <jayala2025@users.noreply.github.com> Co-authored-by: rtelepak <rtelepak@users.noreply.github.com> Co-authored-by: Matvei Popov <Matvezy@users.noreply.github.com> Co-authored-by: lucypham951 <lucypham951@users.noreply.github.com>

--- tic-tac-iffffy.hs
+++ tic-tac-iffffy.hs
@@ -3,7 +3,6 @@ import Data.List.Split
 import Data.Maybe
 import Debug.Trace
 
-
 main :: IO ()
 main = return ()
 
@@ -12,18 +11,19 @@ type Cell = Maybe Player
 data MiniBoard = Game [Cell] | Winner (Maybe Player) deriving (Show, Eq)
 type BigBoard = [MiniBoard] 
 type Turn = Player
+-- Cursor parking [ | M | J😩 | K 🥵| R | L | ]
+-- Cursor cafe [🍊 ]
 type BigBoardIndex = Int
 type MiniBoardIndex = Int
 type GameState = (Turn, BigBoard)
---type Outcome = (Player, MiniBoard)
-type Outcome = Cross | Circle | Ongoing
+data Outcome = Win Player | Ongoing | Tie deriving (Show, Eq)
 type Location = (BigBoardIndex, MiniBoardIndex)
 
 possibleWins = [[0,1,2],[3,4,5],[6,7,8],
                [0,3,6],[1,4,7],[2,5,8],
                [0,4,8],[2,4,6]]
 
-gameStateWinner :: GameState -> Player
+gameStateWinner :: GameState -> Outcome
 gameStateWinner = undefined
 
 makeMove :: GameState -> GameState
@@ -47,6 +47,5 @@ case cell of Nothing -> let newBoards = updateWinners x board
 getLegalMoves :: GameState -> [Location] 
 getLegalMoves = undefined
 
-showGameState :: GameState -> BigBoard 
+showGameState :: GameState -> String --BigBoard 
 showGameState = undefined
-
