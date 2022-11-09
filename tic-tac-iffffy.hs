6f53f05c39749a03e98177c8e9479142ea1a8cc5
Matvei Popov <mpopov@trinity.edu>
2022-11-09 12:33:50 -0800

    code

--- tic-tac-iffffy.hs
+++ tic-tac-iffffy.hs
@@ -6,7 +6,7 @@ import Debug.Trace
 main :: IO ()
 main = return ()
 
-data Player = Cross | Circle deriving (Show, Eq)
+data Player = Cross | Circle deriving (Eq)
 type Cell = Maybe Player
 data MiniBoard = Game [Cell] | Winner (Maybe Player) deriving (Show, Eq)
 type BigBoard = [MiniBoard] 
@@ -16,7 +16,7 @@ type Turn = Player
 type BigBoardIndex = Int
 type MiniBoardIndex = Int
 type GameState = (Turn, BigBoard)
-data Outcome = Win Player | Ongoing | Tie deriving (Show, Eq)
+data Outcome = Win Player | Ongoing BigBoard | Tie deriving (Eq)
 type Location = (BigBoardIndex, MiniBoardIndex)
 
 possibleWins = [[0,1,2],[3,4,5],[6,7,8],
@@ -26,31 +26,76 @@ possibleWins = [[0,1,2],[3,4,5],[6,7,8],
 gameStateWinner :: GameState -> Outcome
 gameStateWinner = undefined
 
-makeMove :: GameState -> GameState
-makeMove = undefined
+updateMatrix :: BigBoard -> Turn -> Location -> BigBoard
+updateMatrix m x (r,c) =
+  let (Game cellsAtC) = m !! r 
+  in take r m ++
+    [Game $ take (c-1) cellsAtC ++ [Just x] ++ drop (c + 1) cellsAtC] ++
+    drop (r + 1) m
 
-getCellOfLocation :: Location -> Cell --checkCell helper, use in legal moves 
-getCellOfLocation loc = undefined
+makeMove :: GameState -> Maybe Location -> GameState
+makeMove (Cross, bboard) (Just loc) = -- for human player
+  case checkCell loc (Cross, bboard) of 
+    True -> (Circle, updateMatrix bboard Cross loc)
+    False -> error "Illegal move"
+makeMove (Circle, bboard) Nothing = -- for computer turn
+  let loc = bestLoc bboard
+  in undefined
 
-checkCell :: Location -> Bool -- legal move helper, need to include gamestate
-checkCell location = 
-  let cell = getCellOfLocation location
-  in case cell of
-          Nothing -> True
-          _ -> False    
-                              
-{-
-case cell of Nothing -> let newBoards = updateWinners x board
-                          in if thereIsWinner 
-                            then celebrate
-                            else waitForNewMove
-             Just x  -> error "Overwriting move"
-  where 
-    cell = getCellOfLocation location 
-  -}
+  where bestLoc :: BigBoard -> Location
+        bestLoc bboard = undefined 
+
+getCellOfLocation :: Location -> GameState -> Either Cell Outcome
+getCellOfLocation (bigIndex, miniIndex) (_,bboard)
+  | bigIndex < 0 || miniIndex < 0 || bigIndex > 8 || miniIndex > 8 = error "IndexOutOfBound in getCellOfLocation"
+  | otherwise =
+      case (bboard !! bigIndex) of 
+        Game cells -> Left $ cells !! miniIndex
+        Winner Nothing        -> Right Tie
+        Winner (Just player)  -> Right (Win player)
+
+checkCell :: Location -> GameState -> Bool
+checkCell location gs = 
+  case getCellOfLocation location gs of 
+          Right _ -> False
+          Left (Nothing) -> True
+          Left (Just player) -> False
 
 getLegalMoves :: GameState -> [Location] 
 getLegalMoves = undefined
 
+emptyBoard = take 9 $ repeat (Game $ take 9 $ repeat Nothing)
+allXBoard = take 9 $ repeat (Game $ take 9 $ repeat (Just Cross))
+
 showGameState :: GameState -> String --BigBoard 
-showGameState = undefined
+showGameState (turn, bigboard) = unlines ["Current turn: " ++ show turn ++ "\n", showBigBoard bigboard]
+
+instance Show Player where
+  show Cross  = " x "
+  show Circle = " o "
+
+-- data MiniBoard = Game [Cell] | Winner (Maybe Player) deriving (Show, Eq)
+
+showMiniBoard :: String -> MiniBoard -> String
+showMiniBoard sep (Game cells) = 
+  intercalate sep $ map (intercalate "|") $ chunksOf 3 $ map showFor cells
+  where showFor :: Cell -> String
+        showFor Nothing = "   "
+        showFor (Just player) = show player
+        
+showMiniBoard sep (Winner Nothing) = 
+  let cells = take 9 $ repeat "Tie"
+  in intercalate sep $ map (intercalate "|") $ chunksOf 3 cells
+
+showMiniBoard sep (Winner (Just p)) = 
+  let cells = take 9 $ repeat $ show p
+  in intercalate sep $ map (intercalate "|") $ chunksOf 3 cells
+  
+showBigBoard :: BigBoard -> String
+showBigBoard miniBoards = 
+  let panels = chunksOf 3 miniBoards
+      panelSeparator = '\n':(take 37 (repeat '-') ++"\n")
+  in (intercalate panelSeparator $ map printPanel panels) ++ "\n"
+  where printPanel :: [MiniBoard] -> String
+        printPanel panel = 
+          intercalate "\n" $ map (intercalate "||") $ transpose $ map (splitOn "\n") $ map (showMiniBoard "\n") panel
