0beaaba2495b2374673806804ef1b33a94d3914e
Matvei Popov <mpopov@trinity.edu>
2022-11-14 14:18:41 -0800

    testing 1

--- Testing.hs
+++ Testing.hs
@@ -1,13 +1,58 @@
 module Testing where
+import Test.QuickCheck
 import Game
 import Test.Hspec
 import Data.List
 import Data.List.Split
 import Data.Maybe
 import Debug.Trace
-main :: IO ()
+import Data.Typeable
+instance Show (a -> b) where
+         show a= "funcion"
 
-main = do  
-    putStrLn "Hello, what's your name?"  
-    name <- getLine  
-    putStrLn ("Hey " ++ name ++ ", you rock!")
+compBoard = (Cross, [(Cross, Game [Just Cross, Just Cross, Just Circle,
+                                     Nothing, Just Cross, Just Circle,
+                                     Just Cross, Just Circle, Just Cross]),
+                     (Cross, Game [Just Cross, Just Cross, Just Cross,
+                                     Just Circle, Just Cross, Just Circle,
+                                     Nothing, Just Cross, Just Circle]), 
+                     (Cross, Game [Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle]),
+                     (Cross, Game [Just Cross, Just Cross, Just Cross,
+                                     Just Circle, Just Cross, Just Circle,
+                                     Nothing, Just Cross, Just Circle]),
+                     (Cross, Game [Just Cross, Just Cross, Just Cross,
+                                     Just Circle, Just Cross, Just Circle,
+                                     Nothing, Just Cross, Just Circle]),
+                     (Cross, Game [Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle]),
+                     (Cross, Game [Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle]),
+                     (Cross, Game [Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle]),
+                     (Cross, Game [Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle,
+                                     Just Cross, Just Cross, Just Circle])])
+
+noneBoard = (Circle, replicate 9 (Nothing, Game (replicate 9 Nothing)))
+circleBoard = (Circle, replicate 9 (Nothing, Game (replicate 9 (Just Circle))))
+crossBoard = (Circle, replicate 9 (Nothing, Game (replicate 9 (Just Cross))))
+--putStrLn (showGameState compBoard " ")
+--putStrLn (showGameState noneBoard " ")    
+    
+main = do
+        describe "Winner of game" $ do -- WINNER TESTING
+                it "cross board" $ do
+                    gameStateWinner crossBoard `shouldBe` Just (Win Cross)
+    {-
+    if (gameStateWinner crossBoard) == Just (Win Cross) then putStrLn "Cross Won"
+    else if (gameStateWinner crossBoard) == Just (Win Circle) then putStrLn "Circle Won"
+    else putStrLn (showOutcome(gameStateWinner crossBoard))
+    if (gameStateWinner circleBoard) == Just (Win Cross) then putStrLn "Cross Won"
+    else if (gameStateWinner circleBoard) == Just (Win Circle) then putStrLn "Circle Won"
+    else putStrLn (showOutcome(gameStateWinner circleBoard))
+    -}
